342821912bcd35c79af18aa02806f16c2422b112
Daniel Solorzano <dsolo.prof@gmail.com>
2023-04-09 19:17:49 -0500

    Implemented update() and newGame() on RandomBot class.

--- src/main/java/com/losing_tictactoe/app/RandomBot.java
+++ src/main/java/com/losing_tictactoe/app/RandomBot.java
@@ -12,7 +12,19 @@ public class RandomBot extends Player{
         Random rng = new Random();
         int randomIndex = rng.nextInt(available.size());
         int position = available.get(randomIndex);
-        System.out.println(this.getName() + " chose position: " + position);
+        this.lastMove = position;
         return position;
+    }
+
+    @Override
+    public void update(Object data) {
+        if(this.getLastMove() == (int) data) {
+            System.out.println(this.getName() + " chose position: " + (int) data);
+        }
+    }
+
+    @Override
+    public void newGame(Player winner) {
+        
     }
 }
